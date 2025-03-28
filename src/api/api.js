f6ed1f772acaae7d38233f8ef13407ef17b69f8d
ducleader2003@gmail.com <ducleader2003@gmail.com>
2025-03-28 11:44:36 +0700

    Update source code.

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = process.env.REACT_APP_BASE_URL;
+const BASE_URL = `${process.env.REACT_APP_BASE_URL}/app`;
 
 //  Cấu hình Axios để dễ dàng tái sử dụng
 const api = axios.create({
